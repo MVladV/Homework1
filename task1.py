24349d11599c33e6303b753609e46e3e272510f7
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2021-09-22 18:29:47 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,9 @@
 # task1
 
-import sys # pass the list of arguments from the console
-
+# pass the list of arguments from the console
+import sys 
+# calculation of mathematical expression
 try:
-    print(eval(' '.join(sys.argv[1]+sys.argv[2]+sys.argv[3]))) # calculation of mathematical expression
+    print(eval(' '.join(sys.argv[1]+sys.argv[2]+sys.argv[3]))) 
 except Exception:
     print("Exeption!")
