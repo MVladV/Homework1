3f1d6629e8b28b2778f11c766e224c4b58fea6cc
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2021-09-22 19:24:27 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,13 +1,32 @@
-import re
+# task3
 
-variables = {}
-formula = input('Insert formula: ')
-while True:
-    try:
-        res = eval(formula, variables)
-    except NameError as e:
-        v = re.match('name .(\w+). is not defined', e.message).group(1)
-        variables[v] = input('insert value for %s: ' % v)
-        continue
-    print ("True, %s" % (res))
-    break
+sign = ('+', '-')
+number = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+symbol = ("number", "function")
+
+#number = true, sign = false
+
+
+def is_legal_statement(expression, symb):
+    if len(expression) == 0:
+        return True
+    elif expression[0] in number and symb == True:
+        symb = False
+        return is_legal_statement(expression[1:], symb)
+    elif expression[0] in sign and symb == False:
+        symb = True
+        return is_legal_statement(expression[1:], symb)
+    else:
+        return False
+
+
+def main():
+    user_string = input("Input expression: ")
+    if len(user_string) > 2:
+        if is_legal_statement(user_string, True):
+            print("(True,", eval(''.join(user_string)), ")")
+        else:
+            print("(False,None)")
+
+
+main()    
