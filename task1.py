44a567c09a45eebfd29bf72b02f37daeb044e18b
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2021-09-22 18:27:20 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
-print(eval(input("Введите математическое выражение: ")))
+# task1
+
+import sys # pass the list of arguments from the console
+
+try:
+    print(eval(' '.join(sys.argv[1]+sys.argv[2]+sys.argv[3]))) # calculation of mathematical expression
+except Exception:
+    print("Exeption!")
