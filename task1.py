765ac23782c4d266696b91f1ac0f13b02ae7aa46
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2021-11-12 12:15:52 +0200

    Delete task1.py

--- task1.py
+++
@@ -1,9 +0,0 @@
-# task1
-
-# pass the list of arguments from the console
-import sys 
-# calculation of mathematical expression
-try:
-    print(eval(' '.join(sys.argv[1]+sys.argv[2]+sys.argv[3]))) 
-except Exception:
-    print("Exeption!")
