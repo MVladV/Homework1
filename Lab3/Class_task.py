3e1c60ba8318043d8eb67ceb8d8e1b3bc169ea38
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2021-11-12 12:07:03 +0200

    Update Class_task.py

--- Lab3/Class_task.py
+++ Lab3/Class_task.py
@@ -1,1 +1,51 @@
+import os
+import random
+import timeit
 
+
+# class Test:
+
+    # str1 = [x ** 2 for x in range(10)]  # заповнення масиву через генератор(швидше)
+    # print(str1)
+    # list = []
+
+    # for i in range(10):  # заповнення масиву через цикл
+    #    list.append(i * i)
+    #    print(list)
+
+    # x = open("test.txt", "w+") # заповнення масиву числами до певного розміру
+    # x.truncate()
+    # while os.path.getsize("test.txt") < 52428800:  # 50 MB
+    # x.write(str(random.randrange(1000)) + "\n")
+
+test = """
+sum = 0
+x = open("test.txt", "r")
+for line in x:
+    if line.strip().isdigit():
+        sum += int(line)
+x.close()
+"""
+
+print(timeit.timeit(test, number=1)) # час роботи
+
+test = """ # найшкидший варіант
+sum = 0
+x = open("test.txt", "r")
+lines = x.readlines()
+for line in lines:
+    if line.strip().isdigit():
+        sum += int(line)
+x.close()
+"""
+
+print(timeit.timeit(test, number=1))
+
+test = """
+file = open("test.txt", "r")
+nums = (int(line.strip()) for line in file if line.strip().isdigit())
+sum1 = sum(nums)
+file.close()
+"""
+
+print(timeit.timeit(test, number=1))
