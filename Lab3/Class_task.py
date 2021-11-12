eb85fe5074ee07137e2117b83a11324c0bbc893b
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2021-11-12 12:25:19 +0200

    Update Class_task.py

--- Lab3/Class_task.py
+++ Lab3/Class_task.py
@@ -19,6 +19,15 @@ import timeit
     # x.write(str(random.randrange(1000)) + "\n")
 
 test = """
+file = open("test.txt", "r")
+nums = (int(line.strip()) for line in file if line.strip().isdigit())
+sum1 = sum(nums)
+file.close()
+"""
+
+print(timeit.timeit(test, number=1))
+    
+test = """
 sum = 0
 x = open("test.txt", "r")
 for line in x:
@@ -37,15 +46,6 @@ for line in lines:
     if line.strip().isdigit():
         sum += int(line)
 x.close()
-"""
-
-print(timeit.timeit(test, number=1))
-
-test = """
-file = open("test.txt", "r")
-nums = (int(line.strip()) for line in file if line.strip().isdigit())
-sum1 = sum(nums)
-file.close()
 """
 
 print(timeit.timeit(test, number=1))
