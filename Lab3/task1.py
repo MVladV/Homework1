6ed287e4fae5df36ce248307b96eaada3124e3fa
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2021-11-24 15:25:58 +0200

    Update task1.py

--- Lab3/task1.py
+++ Lab3/task1.py
@@ -5,23 +5,27 @@
 # to the reguler ticket price. All tickets must have the following properties: -the ability to construct a ticket by
 # number; -the ability to ask for a ticket’s price; -the ability to print a ticket as a String.
 
-import json
+# Write a program for selling tickets to IT-events. Each ticket has a unique number and a price. There are four type_ts
+# of tickets: standard ticket, advance ticket (purchased 60 or more days before the event), late ticket (purchased
+# fewer than 10 days before the event) and student ticket. Additional information: -advance ticket - discount 40% of
+# the standard ticket price; -student ticket - discount 50% of the standard ticket price; -late ticket - additional 10%
+# to the regular ticket price. All tickets must have the following properties: -the ability to construct a ticket by
+# number; -the ability to ask for a ticket’s price; -the ability to print a ticket as a String.
+
+import json  # типи білетів мають бути окремо від класу Event
 
 
 class Events:
 
-    def __init__(self, name_of_file, name, standard, advance, late, student, price):
-        self.price_student = round(price * 0.5)  # студентський
-        self.advance_price = round(price * 0.6)  # завчасно куплений
-        self.late_price = round(price * 1.1)  # куплено запізно
+    def __init__(self, name_of_file, name, tickets, price):
+        # self.price_student = round(price * 0.5)  # студентський (ОКРЕМО)
+        # self.advance_price = round(price * 0.6)  # завчасно куплений
+        # self.late_price = round(price * 1.1)  # куплено запізно
         self.name_of_file = name_of_file
         self.name = name
-        self.standard = standard
-        self.advance = advance
-        self.late = late
-        self.student = student
+        self.tickets = tickets
         self.price = price
-        self.quantity = standard + advance + late + student
+        self.current = tickets
         Events.add_event(self)
 
     @property
@@ -49,79 +53,34 @@ class Events:
         self.__price = price
 
     @property
-    def standard(self):
-        return self.__standard
+    def tickets(self):
+        return self.__tickets
 
-    @standard.setter
-    def standard(self, standard):
-        if not isinstance(standard, int):
+    @tickets.setter
+    def tickets(self, tickets):
+        if not isinstance(tickets, int):
             raise TypeError()
-        if standard <= 0:
+        if tickets <= 0:
             raise ValueError()
-        self.__standard = standard
-
-    @property
-    def advance(self):
-        return self.__advance
-
-    @advance.setter
-    def advance(self, advance):
-        if not isinstance(advance, int):
-            raise TypeError()
-        if advance <= 0:
-            raise ValueError()
-        self.__advance = advance
-
-    @property
-    def late(self):
-        return self.__late
-
-    @late.setter
-    def late(self, late):
-        if not isinstance(late, int):
-            raise TypeError()
-        if late <= 0:
-            raise ValueError()
-        self.__late = late
-
-    @property
-    def student(self):
-        return self.__student
-
-    @student.setter
-    def student(self, student):
-        if not isinstance(student, int):
-            raise TypeError()
-        if student <= 0:
-            raise ValueError()
-        self.__student = student
+        self.__tickets = tickets
 
     def add_event(self):  # add to .json file
 
         event = {
             "Name of file": self.name_of_file,
             "Name": self.name,
-            "Standard quantity": self.standard,
+            "Tickets": self.tickets,
             "Price": self.price,
-            "Advance quantity": self.advance,
-            "Advance price": self.advance_price,
-            "Late quantity": self.late,
-            "Late price": self.late_price,
-            "Student quantity": self.student,
-            "Student price": self.price_student
-        }
+            }
         with open(self.name_of_file, "w") as write_file:
             json.dump(event, write_file)
         write_file.close()
 
     def __str__(self):
         return f"Event: \n" \
-               f"{self.__standard} standard tickets, price - {self.__price}, " \
-               f"{self.__advance} advance tickets, price - {self.advance_price}, " \
-               f"{self.__late} late tickets, price - {self.late_price}, " \
-               f"{self.__student} student tickets, price - {self.price_student} \n"
+               f"{self.__tickets} tickets, price - {self.__price}"
 
-    num_events = []
+    num_standard = 0
     num_st = 0
     num_advance = 0
     num_late = 0
@@ -137,9 +96,13 @@ class Tickets:
         self.days = days
         self.price = price
         self.type_t = type_t
-        Tickets.add_ticket(self)
-        if len(self.event.num_events) > event.quantity:
+        event.num_standard += 1
+        event.current -= 1
+        if not event.current:
             raise ValueError("No more tickets.")
+        Tickets.add_ticket(self)
+
+
 
     @property
     def event(self):
@@ -161,9 +124,6 @@ class Tickets:
             raise TypeError()
         if number <= 0:
             raise ValueError()
-        for ticket in self.event.num_events:
-            if number in ticket.values():
-                raise ValueError()
         self.__number = number
 
     @property
@@ -177,6 +137,12 @@ class Tickets:
         if days <= 0:
             raise ValueError()
         self.__days = days
+
+    @staticmethod
+    def standard_price(name_of_file):
+        with open(name_of_file, "r") as read_file:
+            info = json.load(read_file)
+        return info.get("Price")
 
     def add_ticket(self):  # add to .json file
         ticket = {
@@ -193,7 +159,7 @@ class Tickets:
 
     def __str__(self):
         return f"{self.type_t} ticket #{self.number} for the {self.event.name} event:" \
-               f" price - {self.price}, bought {self.days} days before the event"
+               f" price - {int(self.price)}, bought {self.days} days before the event"
 
     @staticmethod
     def get_num(event, number):
@@ -212,65 +178,54 @@ class Tickets:
             if ticket.get("Event") == event.name and ticket.get("Number") == number:
                 return ticket.get("Price")
 
-    @staticmethod
-    def standard_price(name_of_file):
-        with open(name_of_file, "r") as read_file:
-            info = json.load(read_file)
-        return info.get("Price")
-
 
 class Student(Tickets):
 
-    def __init__(self, event, number, days):
+    def __init__(self, event, number, days, price):
         with open(event.name_of_file, "r") as read_file:
             info = json.load(read_file)
-        super().__init__(event, number, days, info.get("Student price"), "Student ticket")
+        super().__init__(event, number, days,  price * 0.5, "Student ticket")
         event.num_st += 1
-        if event.num_st > event.student:
-            raise ValueError()
 
 
 class Advance(Tickets):
 
-    def __init__(self, event, number, days):
+    def __init__(self, event, number, days, price):
         if days < 60:
             raise ValueError()
         with open(event.name_of_file, "r") as read_file:
             info = json.load(read_file)
-        super().__init__(event, number, days, info.get("Advance price"), "Advance ticket")
+        super().__init__(event, number, days, price * 0.6, "Advance ticket")
         event.num_advance += 1
-        if event.num_advance > event.advance:
-            raise ValueError()
 
 
 class Late(Tickets):
 
-    def __init__(self, event, number, days):
+    def __init__(self, event, number, days, price):
         if days > 10:
             raise ValueError()
         with open(event.name_of_file, "r") as read_file:
             info = json.load(read_file)
-        super().__init__(event, number, days, info.get("Late price"), "Late ticket")
+        super().__init__(event, number, days, price * 1.1, "Late ticket")
         event.num_late += 1
-        if event.num_late > event.late:
-            raise ValueError()
 
 
 def main():
     try:
-        event_1 = Events("event1.json", "Business Analyst course", 10, 2, 2, 5, 700)
+        event_1 = Events("event1.json", "Business Analyst course", 10, 700)
         print(event_1)
 
-        ticket_1 = Tickets(event_1, 1, 15, Tickets.standard_price(event_1.name_of_file))
-        ticket_2 = Tickets(event_1, 2, 16, Tickets.standard_price(event_1.name_of_file))
+        first_price = Tickets.standard_price(event_1.name_of_file)
+        ticket_1 = Tickets(event_1, 1, 15, first_price)
+        ticket_2 = Tickets(event_1, 2, 16, first_price)
         print("Received a ticket by number:")
         print(Tickets.get_num(event_1, 2))
         print()
-        ticket_3 = Late(event_1, 3, 2)
-        ticket_4 = Advance(event_1, 4, 70)
-        ticket_5 = Student(event_1, 5, 10)
-        ticket_6 = Advance(event_1, 6, 67)
-        ticket_7 = Student(event_1, 7, 14)
+        ticket_3 = Late(event_1, 3, 2, first_price)
+        ticket_4 = Advance(event_1, 4, 70, first_price)
+        ticket_5 = Student(event_1, 5, 10, first_price)
+        ticket_6 = Advance(event_1, 6, 67, first_price)
+        ticket_7 = Student(event_1, 7, 14, first_price)
 
         print("All tickets:")
         print(ticket_1)
@@ -280,6 +235,9 @@ def main():
         print(ticket_5)
         print(ticket_6)
         print(ticket_7)
+
+        print("\nGet price by number:")
+        print(Tickets.get_price(event_1, 5))
 
     except Exception:
         print("Exeption!")
